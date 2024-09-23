2cdd5a72eea76c51982ee1367d468c287ac03cc2
Koushik <programmer.koushik@gmail.com>
2024-09-23 11:56:08 +0530

    privacy policy page

---
+++ src/app/(base)/privacy-policy/page.tsx
@@ -0,0 +1,223 @@
+import { Minus } from "lucide-react";
+
+export default function page() {
+  return (
+    <main className="container py-24">
+      <h1 className="text-4xl text-gray-800">Privacy Policy</h1>
+      <p className="italic my-5 text-gray-700">Effective Date: July 1, 2024</p>
+
+      {/* Introduction */}
+      <h2 className="text-2xl text-gray-800">1. Introduction</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        Welcome to DreamRP Inc. (&quot;
+        <span className="font-semibold text-gray-700">we</span>&quot;, &quot;
+        <span className="font-semibold text-gray-700">us</span>&quot;, or &quot;
+        <span className="font-semibold text-gray-700">our</span>&quot;). DreamRP
+        Inc. is committed to protecting your privacy and ensuring you have a
+        positive experience on our website and in using our products and
+        services (&quot;
+        <span className="font-semibold text-gray-700">Services</span>&quot;).
+        This Privacy Policy outlines how we collect, use, disclose, and
+        safeguard your information when you visit our website or use our
+        Services.
+      </p>
+      <p className="text-gray-500 leading-loose mt-6">
+        By accessing or using our Services, you agree to the terms of this
+        Privacy Policy.
+      </p>
+
+      {/* Information We Collect */}
+      <h2 className="text-2xl mt-12">2. Information We Collect</h2>
+
+      <h3 className="text-xl mt-6 mb-4">Personal Information</h3>
+      <p className="text-gray-500 leading-loose">
+        We may collect personally identifiable information that you provide to
+        us when you register on our site, subscribe to our newsletter, make a
+        purchase, fill out a form, or interact with our Services. This
+        information may include, but is not limited to:
+      </p>
+      <ul className="mt-3 text-gray-500">
+        <li className="flex items-center gap-4">
+          <Minus className="w-4 h-auto" /> <>Name</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Email address</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Billing and mailing address</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Phone number</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Payment information</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Social media handles</>
+        </li>
+      </ul>
+
+      <h3 className="text-xl mt-6 mb-4">Non-Personal Information</h3>
+      <p className="text-gray-500 leading-loose">
+        We may also collect non-personally identifiable information that does
+        not directly identify you. This may include:
+      </p>
+      <ul className="mt-3 text-gray-500">
+        <li className="flex items-center gap-4">
+          <Minus className="w-4 h-auto" /> <>Browser and device information</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>IP address</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Pages visited on our website</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Time and date of visit</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Referring website</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" /> <>Social media handles</>
+        </li>
+      </ul>
+
+      {/* How We Use Your Information */}
+      <h2 className="text-2xl mt-12">3. How We Use Your Information</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        We use the information we collect to:
+      </p>
+      <ul className="mt-3 text-gray-500">
+        <li className="flex items-center gap-4">
+          <Minus className="w-4 h-auto" />{" "}
+          <>Provide, operate, and maintain our Services</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" />{" "}
+          <>Improve, personalize, and expand our Services</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" />{" "}
+          <>
+            Process transactions and send related information, such as purchase
+            confirmations
+          </>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" />{" "}
+          <>
+            Communicate with you, including sending you updates, newsletters,
+            and marketing materials
+          </>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" />{" "}
+          <>Monitor and analyze usage and trends to improve user experience</>
+        </li>
+        <li className="flex items-center gap-4 mt-2">
+          <Minus className="w-4 h-auto" />{" "}
+          <>
+            Comply with legal obligations and protect the safety, rights, and
+            property of DreamRP Inc., our users, and the public
+          </>
+        </li>
+      </ul>
+
+      {/* Sharing Your Information */}
+      <h2 className="text-2xl mt-12">4. Sharing Your Information</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        We do not sell, trade, or otherwise transfer your personally
+        identifiable information to outside parties except as described below:
+      </p>
+      <ul className="mt-3 text-gray-500">
+        <li className="flex items-start gap-4">
+          <Minus className="w-4 h-auto mt-[6px]" />{" "}
+          <p>
+            <span className="font-semibold text-gray-700 italic">
+              Service Providers:
+            </span>
+            {"   "}
+            We may share your information with third-party service providers
+            that assist us in operating our Services, conducting our business,
+            or serving our users, as long as those parties agree to keep this
+            information confidential.
+          </p>
+        </li>
+        <li className="flex items-start gap-4 mt-2">
+          <Minus className="w-4 h-auto mt-[6px]" />{" "}
+          <p>
+            <span className="font-semibold text-gray-700 italic">
+              Legal Requirements:
+            </span>
+            {"   "}
+            We may disclose your information if required to do so by law or in
+            response to valid requests by public authorities (e.g., a court or
+            government agency).
+          </p>
+        </li>
+        <li className="flex items-start gap-4 mt-2">
+          <Minus className="w-4 h-auto mt-[6px]" />{" "}
+          <p>
+            <span className="font-semibold text-gray-700 italic">
+              Business Transfers:
+            </span>
+            {"   "}
+            In the event of a merger, acquisition, or sale of all or a portion
+            of our assets, your information may be transferred to the acquiring
+            entity.
+          </p>
+        </li>
+      </ul>
+
+      {/* Cookies and Tracking Technologies */}
+      <h2 className="text-2xl mt-12">5. Cookies and Tracking Technologies</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        We may use cookies, web beacons, tracking pixels, and other tracking
+        technologies to collect information about your activities on our website
+        and interactions with our Services. This helps us provide you with a
+        better experience, analyze trends, and administer the site.
+      </p>
+
+      {/* Data Security */}
+      <h2 className="text-2xl mt-12">6. Data Security</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        We take the security of your personal information seriously and
+        implement appropriate technical and organizational measures to protect
+        it from unauthorized access, use, or disclosure. However, no method of
+        transmission over the internet or method of electronic storage is
+        completely secure, and we cannot guarantee absolute security.
+      </p>
+
+      {/* Your Rights and Choices */}
+      <h2 className="text-2xl mt-12">7. Your Rights and Choices</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        We take the security of your personal information seriously and
+        implement appropriate technical and organizational measures to protect
+        it from unauthorized access, use, or disclosure. However, no method of
+        transmission over the internet or method of electronic storage is
+        completely secure, and we cannot guarantee absolute security.
+      </p>
+
+      {/* Children’s Privacy */}
+      <h2 className="text-2xl mt-12">8. Children&apos;s Privacy</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        Our Services are not intended for children under the age of 13. We do
+        not knowingly collect or solicit personal information from children
+        under 13. If we become aware that we have collected personal information
+        from a child under the age of 13, we will take steps to delete that
+        information.
+      </p>
+
+      {/* Changes to This Privacy Policy */}
+      <h2 className="text-2xl mt-12">9. Changes to This Privacy Policy</h2>
+      <p className="text-gray-500 leading-loose mt-6">
+        We may update this Privacy Policy from time to time. We will notify you
+        of any changes by posting the new Privacy Policy on our website with the
+        &quot;Effective Date&quot; at the top. We encourage you to review this
+        Privacy Policy periodically to stay informed about how we are protecting
+        your information.
+      </p>
+    </main>
+  );
+}
